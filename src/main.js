8ceb096136257715543cd725ff8e99999166c0d5
Ece Özmen <145540611+ece-oez@users.noreply.github.com>
2025-02-21 12:01:20 +0000

    install bootstrap icons

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import App from './App.vue'
 import router from './router'
+
 
 const app = createApp(App)
 
