6fa2232ed47cbf1d73a329f616a562fa406c7ae4
Ece Özmen <145540611+ece-oez@users.noreply.github.com>
2025-02-25 07:48:26 +0000

    einzelne views erstellt für routerview

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,33 +1,21 @@
 <script setup>
-import TheMenu from '@/components/TheMenu.vue'
-import TopBar from '@/components/TopBar.vue'
 import WahlKarten from '@/components/WahlKarten.vue'
 import { useMenuStore } from '@/stores/counter'
+import { RouterLink } from 'vue-router'
 
 const menuStore = useMenuStore()
 </script>
 
 <template>
-  <div class="h-screen flex flex-col">
-    <TopBar class="w-full p-3"></TopBar>
-    <div class="flex flex-row h-full">
-      <div
-        id="menu"
-        v-if="menuStore.menuBtnState"
-        class="w-1/5 border-e-2 border-stone-400 shadow-2xl"
-      >
-        <TheMenu />
-      </div>
-      <div class="w-full h-full flex justify-around items-center">
-        <WahlKarten icon="bi bi-eyeglasses text-6xl" text="Daten Lesen"></WahlKarten>
-        <!-- <WahlKarten icon="bi bi-pen" text="Daten Bearbeiten"></WahlKarten> -->
-        <WahlKarten
-          icon="bi bi-plus-circle-dotted text-5xl"
-          text="Daten Erstellen"
-          class="gap-4"
-        ></WahlKarten>
-      </div>
-    </div>
+  <div class="w-full h-full flex justify-around items-center">
+    <RouterLink to="/anzeigen" class="w-1/5 h-1/4">
+      <WahlKarten icon="bi bi-eyeglasses text-6xl" text="Daten Anzeigen" />
+    </RouterLink>
+
+    <!-- <WahlKarten icon="bi bi-pen" text="Daten Bearbeiten"></WahlKarten> -->
+    <RouterLink to="/erstellen" class="w-1/5 h-1/4">
+      <WahlKarten icon="bi bi-plus-circle-dotted text-5xl" text="Daten Erstellen" class="gap-4" />
+    </RouterLink>
   </div>
 </template>
 
---
+++ src/views/WindowView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import TheMenu from '@/components/TheMenu.vue'
+import TopBar from '@/components/TopBar.vue'
+import { useMenuStore } from '@/stores/counter'
+
+const menuStore = useMenuStore()
+</script>
+
+<template>
+  <div class="h-screen flex flex-col">
+    <TopBar class="w-full p-3"></TopBar>
+    <div class="flex flex-row h-full">
+      <div
+        id="menu"
+        v-if="menuStore.menuBtnState"
+        class="w-1/5 border-e-2 border-stone-400 shadow-2xl"
+      >
+        <TheMenu />
+      </div>
+
+      <RouterView class="w-full h-full"></RouterView>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
