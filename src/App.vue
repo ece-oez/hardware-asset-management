898b5064336bb981e42c1bae393c5c6c09ec3eca
oezmen <oezmen@roller.de>
2025-03-16 10:35:42 +0100

    login form wip

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
 import TheLogin from './components/TheLogin.vue'
 import TheModal from './components/TheModal.vue'
-import { useModalStore } from './stores/modal'
 import WindowView from './views/WindowView.vue'
-
-const modalStore = useModalStore()
 </script>
 
 <template>
