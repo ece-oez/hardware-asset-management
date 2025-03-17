73fb16efba1835ef788d20281434f37852a54c28
Ece Oezmen <ece.2002.67@gmail.com>
2025-03-17 15:25:30 +0100

    ui finished

--- src/views/DokumentationView.vue
+++ src/views/DokumentationView.vue
@@ -1,8 +1,6 @@
 <script setup>
 import ConstructionSite from '@/components/site_overlay/ConstructionSite.vue'
-import LoadingSite from '@/components/site_overlay/LoadingSite.vue'
 </script>
 <template>
-  <!-- <ConstructionSite><div class="font-bold">"Dokumentation"</div></ConstructionSite> -->
-  <LoadingSite><div class="font-bold">"Dokumentation"</div></LoadingSite>
+  <ConstructionSite><div class="font-bold">"Dokumentation"</div></ConstructionSite>
 </template>
