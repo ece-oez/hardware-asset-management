0ff3e6e92253d9e6bed852ba5f9417f33ffb38aa
oezmen <oezmen@roller.de>
2025-03-18 08:12:26 +0100

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { computed } from 'vue'
+import { onMounted } from 'vue'
 import TheLogin from './components/TheLogin.vue'
 import TheModal from './components/TheModal.vue'
 import WindowView from './views/WindowView.vue'
-import { useRouter } from 'vue-router'
 const loginState = false
 </script>
 
