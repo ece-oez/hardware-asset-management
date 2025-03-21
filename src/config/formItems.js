6ca5afc70704a1f617b8c9590932f4ec604cdc70
AllEchse <alexporwol@yahoo.com>
2025-03-21 13:14:41 +0100

    create form

---
+++ src/config/formItems.js
@@ -0,0 +1,26 @@
+export const formItems = [
+  {
+    label: "Name"
+    , id: "name"
+  }
+  , {
+    label: "Modellnummer"
+    , id: "modellNumber"
+  }
+  , {
+    label: "Seriennummer"
+    , id: "serialNumber"
+  }
+  , {
+    label: "Erfassungsdatum"
+    , id: "date"
+  }
+  , {
+    label: "Kategorie"
+    , id: "category"
+  }
+  , {
+    label: "Ort"
+    , id: "location"
+  }
+]
--- src/views/ErstellenView.vue
+++ src/views/ErstellenView.vue
@@ -1,2 +1,16 @@
-<script setup></script>
-<template>erstellen</template>
+<script setup>
+import { formItems } from '@/config/formItems';
+import containedComponentsList from '@/components/containedComponentsList.vue';
+
+</script>
+<template>
+  <div>
+    <div v-for="formItem in formItems" class="grid grid-cols-2 gap-4">
+      <div :key="formItem.label" class="flex flex-col w-[200px] p-1">
+        <label>{{ formItem.label }}</label>
+        <input type="text" class="w-full border-1" />
+      </div>
+    </div>
+    <containedComponentsList />
+  </div>
+</template>
